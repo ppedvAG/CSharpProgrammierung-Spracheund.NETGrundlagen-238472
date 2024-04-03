a9b8c04453f9e57b5784314359b37081d8911d8a
ppedvLukasK <lukask@ppedv.de>
2024-04-03 15:34:00 +0200

    M007

---
+++ M007/Person.cs
@@ -0,0 +1,157 @@
+﻿namespace M007;
+
+/// <summary>
+/// class
+/// Stellt die Struktur von den fertigen Objekten dar
+/// Enthält Felder, Properties, Methoden, Konstruktoren, Destruktor
+/// Wenn aus dieser Klasse ein Objekt erstellt wird, bekommt dieses Objekt alle definierten Member der Klasse
+/// 
+/// WICHTIG: Innerhalb einer Klasse sollten keine konkreten Werte definiert werden
+/// </summary>
+public class Person
+{
+	#region Feld
+	/// <summary>
+	/// private: Kann nur innerhalb der Klasse angegriffen werden
+	/// </summary>
+	private string vorname;
+
+	/// <summary>
+	/// Get-Methode gibt nur den Wert zurück
+	/// </summary>
+	public string GetVorname()
+	{
+		return vorname;
+	}
+
+	/// <summary>
+	/// Set-Methode schreibt einen neuen Wert in das Feld hinein
+	/// Bietet u.a. Sicherheit (z.B. Überprüfungen)
+	/// </summary>
+	public void SetVorname(string vorname)
+	{
+		if (vorname.Length >= 3 && vorname.Length <= 15 && vorname.All(char.IsLetter))
+		{
+			this.vorname = vorname; //this: Greife aus der Methode hinaus, wird verwendet wenn zwei Felder den gleichen Namen haben
+		}
+		else
+			Console.WriteLine("Name darf nur aus Buchstaben bestehen und muss zw. 3 und 15 Zeichen haben!");
+	}
+	#endregion
+
+	#region Eigenschaft/Property
+	private string nachname;
+
+	/// <summary>
+	/// Property
+	/// Vereinfacht Get-/Set Methoden
+	/// Selbiges wie Get/Set, aber einfacher in der Verwendung
+	/// </summary>
+	public string Nachname
+	{
+		get
+		{
+			return nachname;
+		}
+		set
+		{
+			//Oben: SetVorname(string vorname), Hier: set + value Keyword
+			if (value.Length >= 3 && value.Length <= 15 && value.All(char.IsLetter))
+				nachname = value;
+			else
+				Console.WriteLine("Name darf nur aus Buchstaben bestehen und muss zw. 3 und 15 Zeichen haben!");
+		}
+	}
+
+	//Typen von Properties
+	//- Full-Property: Property mit Getter und Setter + private Feld dahinter (siehe oben)
+	//- Auto-Property: Property ohne private Feld mit { get; set; }
+	//- Get-Only-Property: Property ohne Setter, welches nur einen Wert berechnet/zurückgibt/weitergibt
+
+	//Anwendungsfälle:
+	//GUI Verbindung zw. Backend und Oberfläche
+	//Serialisierung bei XML, JSON
+	//Access Modifier (private, ...) auf den Setter geben
+	public int Alter { get; private set; } = 20;
+
+	public string VollerName
+	{
+		get
+		{
+			return vorname + " " + nachname;
+		}
+	}
+
+	public string VollerName2
+	{
+		get => vorname + " " + nachname;
+	}
+
+	public string VollerName3 => vorname + " " + nachname;
+	#endregion
+
+	#region Funktionen
+	/// <summary>
+	/// Funktionen beziehen sich auf das fertige Objekt
+	/// Wenn eine Funktion aufgerufen werden soll, MUSS ein Objekt dieser Klasse existieren
+	/// Jede Funktion hat Zugriff auf die Werte des Objekts von dem sie aufgerufen wird
+	/// p.Print() -> p.VollerName, p.Alter
+	/// 
+	/// WICHTIG: Nicht static benutzen
+	/// static: Global, bezieht sich nicht auf die umliegende Klasse/Objekt
+	/// </summary>
+	public void Print()
+	{
+        Console.WriteLine($"Mein Name ist {VollerName} und ich bin {Alter} Jahre alt.");
+    }
+    #endregion
+
+    #region Konstruktor
+	/// <summary>
+	/// Konstruktor
+	/// Initialcode bei Erstellung (per new) des Objekts
+	/// Wird immer ausgeführt, wenn das Objekt erstellt wird
+	/// Standardmäßig gibt es immer den leeren Konstruktor
+	/// </summary>
+    public Person()
+	{
+        Console.WriteLine("Person wurde erstellt");
+		Zaehler++;
+    }
+
+	/// <summary>
+	/// Hier können jetzt per Parameter Standardwerte verlangt werden
+	/// Es darf jetzt keine Person mehr angelegt werden ohne vorname und nachname
+	/// 
+	/// Wenn ein eigener Konstruktor angelegt wird, wird der Standardkonstruktor gelöscht
+	/// </summary>
+	public Person(string vorname, string nachname) : this()
+	{
+		this.vorname = vorname;
+		this.nachname = nachname;
+
+		//SetVorname(vorname);
+		//Nachname = nachname;
+	}
+
+	/// <summary>
+	/// Konstruktoren verketten
+	/// Mit this(...) können mehrere Konstruktoren miteinander verkettet werden
+	/// D.h. wenn dieser Konstruktor aufgerufen wird, wird der darüber auch aufgerufen
+	/// </summary>
+	public Person(string vorname, string nachname, int alter) : this(vorname, nachname) //Hier müssen die Werte des verketteten Konstruktors mitgegeben werden
+	{
+		//this.vorname = vorname;
+		//this.nachname = nachname;
+		Alter = alter;
+	}
+    #endregion
+
+	~Person()
+	{
+        Console.WriteLine("Person zerstört");
+    }
+
+	//Global
+	public static int Zaehler { get; private set; }
+}
---
+++ M007/Program.cs
@@ -0,0 +1,105 @@
+﻿namespace M007;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		#region GC
+		for (int i = 0; i < 5; i++)
+		{
+			Person p = new Person();
+		}
+		GC.Collect();
+		GC.WaitForPendingFinalizers();
+
+		//Enumerable.Range(0, 1_000_000_000).ToList(); //4GB anlegen und löschen
+		#endregion
+
+		#region Static
+		//static: Global, separat von Objekten
+
+		//Console ist static, existiert nur einmal
+		//Console c = new Console(); //Nicht möglich, weil static
+		//c.WriteLine();
+		Console.WriteLine();
+
+		//Person.Print(); //Nicht möglich, weil sich nicht-statische Methoden immer auf ein Objekt beziehen
+		Person p2 = new Person();
+		p2.Print();
+
+		Console.WriteLine(DateTime.Now); //Jetzt-Zeit ist einfach da
+
+		//Aufgabe: Personenzähler anlegen
+		//Sollte in Person.cs sein, kann aber auch in Program.cs sein
+		Person p3 = new Person();
+
+        Console.WriteLine(Person.Zaehler);
+		#endregion
+
+		#region Datumswerte
+		//Zwei Klassen: DateTime, TimeSpan
+		DateTime d = new DateTime(2024, 4, 3);
+        Console.WriteLine(d.Day);
+        Console.WriteLine(d.Month);
+        Console.WriteLine(d.Year);
+        Console.WriteLine(d.DayOfWeek); //Wednesday
+        Console.WriteLine(d.DayOfYear); //94
+
+		d += new TimeSpan(180, 0, 0, 0);
+		d -= TimeSpan.FromDays(180);
+        Console.WriteLine(d);
+
+        //Aufgabe: Wieviele Tage bin ich alt?
+        Console.WriteLine(DateTime.Now - new DateTime(1998, 7, 18));
+		#endregion
+
+		#region Werte-/Referenztypen
+		//class und struct
+
+		//class
+		//Referenztyp
+		//Wenn ein Objekt einer Klasse in eine Variable geschrieben wird, wird ein Zeiger angelegt auf das Objekt
+		Person p4 = new Person(); //Hier wird ein Objekt angelegt und ein Zeiger in p4 geschrieben
+		Person p5 = p4; //Hier wird ein Zeiger auf das Objekt hinter p4 gelegt
+		p5.Nachname = "XYZ";
+
+        //Wenn zwei Objekte von Referenztypen verglichen werden, werden die Speicheradressen verglichen
+        Console.WriteLine(p4 == p5);
+        Console.WriteLine(p4.GetHashCode());
+        Console.WriteLine(p5.GetHashCode());
+
+		//struct
+		//Wertetyp
+		//Wenn ein Objekt eines Structs in eine Variable geschrieben wird, wird der Wert abgelegt
+		int original = 10; //Hier wird ein Objekt angelegt und der Wert wird in die Variable geschrieben
+		int x = original; //Hier wird der Inhalt von original in x kopiert
+		x = 50;
+
+		//Wenn zwei Objekte von Wertetypen verglichen werden, werden die Inhalte verglichen
+		Console.WriteLine(original == x);
+		Console.WriteLine(original.GetHashCode());
+		Console.WriteLine(x.GetHashCode());
+
+		ref int y = ref x; //Structs referenzieren
+		y = 100; //Hier wird x auch verändert
+		#endregion
+
+		#region Null
+		int z; //Leer, 0
+		Person p6 = null; //Leer, null
+
+		if (p6 != null) //Wenn in p6 Inhalt enthalten ist ...
+			Console.WriteLine(p6.Alter);
+
+		Person[] personen = new Person[1000]; //Array mit 1000 leeren Plätzen
+
+        Console.WriteLine(p6?.Alter); //Kurzer Null-Check
+
+		//z = null;
+		int? a; //? am Ende eines Typen: Nullable Typ
+		a = null;
+        #endregion
+    }
+
+	//static void Test(Person x!!) { }
+}
